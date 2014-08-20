2effb243dfdee567a486a34ab0e51d40e953310a
Charles Kerr <charles.kerr@canonical.com>
2014-08-19 22:13:45 -0500

    initial pass at 'hello world' src + po

---
+++ src/main.cpp
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2014 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3, as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranties of
+ * MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *   Charles Kerr <charles.kerr@canonical.com>
+ */
+
+#include <glib/gi18n.h> // bindtextdomain()
+#include <gio/gio.h>
+
+#include <locale.h>
+
+int
+main(int /*argc*/, char** /*argv*/)
+{
+    // Work around a deadlock in glib's type initialization.
+    // It can be removed when https://bugzilla.gnome.org/show_bug.cgi?id=674885 is fixed.
+    g_type_ensure(G_TYPE_DBUS_CONNECTION);
+
+    // boilerplate i18n
+    setlocale(LC_ALL, "");
+    bindtextdomain(GETTEXT_PACKAGE, GNOMELOCALEDIR);
+    textdomain(GETTEXT_PACKAGE);
+
+    auto loop = g_main_loop_new(nullptr, false);
+
+    // run until we lose the busname
+//    auto model = std::make_shared<MutableModel>();
+ //   auto world = std::shared_ptr<World>(new DBusWorld(model));
+  //  auto controller = std::make_shared<Controller>(model, world);
+   // GMenuView menu_view (model, controller);
+    // FIXME: listen for busname-lost
+    g_main_loop_run(loop);
+
+    // cleanup
+    g_main_loop_unref(loop);
+    return 0;
+}
