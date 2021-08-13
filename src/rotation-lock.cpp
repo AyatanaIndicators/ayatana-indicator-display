7b110fc4aee889bd5cbec9ed74f83bcde8a50c5b
Mike Gabriel <mike.gabriel@das-netzwerkteam.de>
2021-08-13 09:35:56 +0200

    Revert "Fix schema path"
    This reverts commit fb3029c0a3121b83f1bca05006e5f95512c709e2.

--- src/rotation-lock.cpp
+++ src/rotation-lock.cpp
@@ -198,7 +198,7 @@ private:
 #ifdef HAS_UBUNTU_TOUCH_SCHEMA
   static constexpr char const * m_schema_name {"com.ubuntu.touch.system"};
 #else
-  static constexpr char const * m_schema_name {"org.ayatana.indicator.display"};
+  static constexpr char const * m_schema_name {"org.ayatana.display"};
 #endif
   GSettings* m_settings = nullptr;
   GSimpleActionGroup* m_action_group = nullptr;
