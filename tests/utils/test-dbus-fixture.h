f1dde7a9245a257eac50c902b2080dcd25ba22c5
Mike Gabriel <mike.gabriel@das-netzwerkteam.de>
2024-05-04 09:00:46 +0000

    cppcheck: Fix missingOverride complaints.

--- tests/utils/test-dbus-fixture.h
+++ tests/utils/test-dbus-fixture.h
@@ -30,7 +30,7 @@ class TestDBusFixture: public GlibFixture
   public:
 
     explicit TestDBusFixture() {};
-    virtual ~TestDBusFixture() =default;
+    virtual ~TestDBusFixture() override =default;
 
     explicit TestDBusFixture(const std::vector<std::string>& service_dirs_in): service_dirs(service_dirs_in) {}
 
